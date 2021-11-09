a4b21ac272f98e72ae6058bb6e6a7b3e51af30ad
silviadelya <silviadelyaheryani31@gmail.com>
2021-11-09 22:36:14 +0700

    program

---
+++ Latihan1.py
@@ -0,0 +1,9 @@
+print("Program Mencari Bilangan Terbesar")
+print()
+angka_pertama=int(input("Angka Pertama\t:"))
+angka_kedua=int(input("Angka Kedua\t:"))
+print()
+if (angka_pertama > angka_kedua):
+    print("Angka pertama lebih besar dari angka kedua")
+elif (angka_kedua > angka_pertama):
+        print("Angka kedua lebih besar dari angka pertama")
