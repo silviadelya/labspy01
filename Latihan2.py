1281e101c6c8b70f096c0f9e6bb954c545edfd58
silviadelya <silviadelyaheryani31@gmail.com>
2021-11-09 22:38:39 +0700

    program

---
+++ Latihan2.py
@@ -0,0 +1,11 @@
+print("Program Mengurutkan Data")
+print()
+angka_pertama=input("Bilangan ke-1\t:")
+angka_kedua=input("Bilangan ke-2\t:")
+angka_ketiga=input("Bilangan ke-3\t:")
+angka_keempat=input("Bilangan ke-4\t:")
+bilangan=[angka_pertama, angka_kedua, angka_ketiga, angka_keempat]
+bilangan.sort()
+print("Urutan Bilangan\t:", bilangan)
+
+
